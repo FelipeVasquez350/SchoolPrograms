77f371ee86cddaeb170e80f79d19e9556ff4497f
FelipeVasquez350 <felipe.vasquez.lazzara@gmail.com>
2020-11-10 11:30:28 +0100

    Update Main.java

--- ProgrammaJava 10112020/Main.java
+++ ProgrammaJava 10112020/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
   
-        System.out.println("Hello");
+        System.out.println("Inserisci la base e l'altezza:\n Base: ");
+        Scanner Scan1 = new Scanner(System.in);
+        int Base = Scan1.nextInt();
 
+
+        System.out.println("Altezza: ");
+        Scanner Scan2 = new Scanner(System.in);
+        int Altezza = Scan2.nextInt();
+        
+        Scan1.close();
+        Scan2.close();
+
+        System.out.println("L'area è: " + Base*Altezza);
+        System.out.println("La diagonale è: " + Math.sqrt(Math.pow(Base,2) + Math.pow(Altezza,2)));
+        System.out.println("Il perimetro è: " + (Base*2 + Altezza*2));
     } 
     
 }
