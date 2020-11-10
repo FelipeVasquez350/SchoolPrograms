388d833e9eeed3bf5538d8ce0e8274e61c41c757
FelipeVasquez350 <felipe.vasquez.lazzara@gmail.com>
2020-11-10 13:10:53 +0100

    commit on master

--- ProgrammaJava 10112020/Rettangolo.java
+++ ProgrammaJava 10112020/Rettangolo.java
@@ -4,15 +4,15 @@ public class Rettangolo {
     public int Altezza;
 
     public void Area() {
-        System.out.println("L'area è: " + Base*Altezza);
+        System.out.println("L'area è: " + Base*Altezza + "\n");
     }
 
     public void Perimetro() {
-        System.out.println("La diagonale è: " + Math.sqrt(Math.pow(Base,2) + Math.pow(Altezza,2)));
+        System.out.println("La diagonale è: " + Math.sqrt(Math.pow(Base,2) + Math.pow(Altezza,2)) + "\n");
     }
     
     public void Diagonale() {
-        System.out.println("Il perimetro è: " + (Base*2 + Altezza*2));
+        System.out.println("Il perimetro è: " + (Base*2 + Altezza*2) + "\n");
     }
     
 }
